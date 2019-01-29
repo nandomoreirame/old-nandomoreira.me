052023718b59f1b0aca11777bebbfd6b955d2634
Fernando Moreira <nandomoreira.me@gmail.com>
2019-01-29 01:09:41 -0200

    🎉

--- store/actions.js
+++ store/actions.js
@@ -1,3 +1,10 @@
 export const setPosts = ({ commit, state }) => {
   commit('SET_POSTS', state)
 }
+
+export const setRepos = ({ commit, state }) => {
+  const githubRepos = require('github-repositories')
+  githubRepos('nandomoreirame').then(data => {
+    commit('SET_REPOS', data)
+  })
+}
